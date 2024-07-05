11e3d2c43dbbdd7005443f1bf48a85c50655bdc9
filipcecelja <filip.cecelja@porsche-ep.com>
2024-07-05 12:32:14 +0200

    Initial Commit

---
+++ testWorkshop/testWorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  testWorkshopApp.swift
+//  testWorkshop
+//
+//  Created by Filip Cecelja on 05.07.2024..
+//
+
+import SwiftUI
+
+@main
+struct testWorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
